b6f566d130e4ee70f11e704f3c8b88becec0fbed
aMOPel <momoPFL@gmail.com>
2025-01-24 19:18:30 +0100

    feat: added template for plugins configs

---
+++ home-manager/modules/nvim/plugins/_plugin_config_template.lua
@@ -0,0 +1,14 @@
+table.insert(plugins, {
+	name = "",
+	setup = function() end,
+	config = function() end,
+})
+
+local p = require("utils").p
+
+local M = function(use)
+	use({
+		p(""),
+	})
+end
+return M
