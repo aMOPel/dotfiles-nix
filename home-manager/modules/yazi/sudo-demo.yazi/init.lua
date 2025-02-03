acce1dc2aa035210f0544d48f7c7d61c01091f64
aMOPel <momoPFL@gmail.com>
2025-02-03 21:40:44 +0100

    feat: added improvements to yazi sudo plugin

--- home-manager/modules/yazi/sudo-demo.yazi/init.lua
+++ home-manager/modules/yazi/sudo-demo.yazi/init.lua
@@ -1,3 +1,16 @@
+local function notify(level, ...)
+	local content = ...
+	if type(...) == "table" then
+		content = table.concat(..., " | ")
+	end
+	ya.notify({
+		title = "sudo",
+		content = content,
+		timeout = 10,
+		level = level,
+	})
+end
+
 --- Verify if `sudo` is already authenticated
 --- @return boolean
 local sudo_already = function()
@@ -10,40 +23,24 @@ end
 --- Run a program with `sudo` privilege
 --- @param program string
 --- @param args table
---- @return Output|nil output
---- @return integer|nil err
+--- @return Output|string output
 ---  nil: no error
 ---  1: sudo failed
 local run_with_sudo = function(program, args, cwd)
-	ya.err("HERE", program, args, cwd)
-	-- local cmd = Command("sudo"):args({ program, table.unpack(args) }):cwd(cwd)
-	-- if sudo_already() then
-	-- 	return cmd:output()
-	-- end
-	--
-	-- local permit = ya.hide()
-	-- print(
-	-- 	string.format(
-	-- 		"Sudo password required to run: `%s %s`",
-	-- 		program,
-	-- 		table.concat(args)
-	-- 	)
-	-- )
-	-- local output = cmd:output()
-	-- permit:drop()
-	--
-	-- if output.status.success or sudo_already() then
-	-- 	return output
-	-- end
-	-- return nil, 1
-end
+	local cmd =
+		Command("sudo"):args({ "--", program, table.unpack(args) }):cwd(cwd)
+	if sudo_already() then
+		local permit = ya.hide()
+		local output = cmd:output()
+		permit:drop()
+		return output
+	end
 
-local function info(content)
-	return ya.notify({
-		title = "sudo",
-		content = content,
-		timeout = 5,
-	})
+	local permit = ya.hide()
+	print(string.format("Sudo password required to run: `%s`", program))
+	local output = cmd:output()
+	permit:drop()
+	return output
 end
 
 local selected_or_hovered = ya.sync(function(state)
@@ -74,9 +71,9 @@ local is_cut = ya.sync(function(state)
 end)
 
 local entry = function(state, job)
-	ya.err(job.args)
 	if not job.args then
-		return info("bad")
+		notify("error", "plugin needs arguments")
+		return
 	end
 	local verb = job.args[1]
 
@@ -93,41 +90,48 @@ local entry = function(state, job)
 	local command = ""
 	local args = {}
 
-	if verb == "remove" and files then
-		if job.args.permanently then
-			command = "rm"
-			args = { "-rI", table.unpack(files) }
-		else
-			command = "trash-cli"
-			args = { "--", table.unpack(files) }
+	if verb == "remove" then
+		if files then
+			if job.args.permanently then
+				command = "rm"
+				args = { "-r", "--", table.unpack(files) }
+			else
+				command = "trash-cli"
+				args = { "--", table.unpack(files) }
+			end
 		end
-	elseif yanked and verb == "paste" then
-		if is_cut() then
-			command = "mv"
-			local force = "-i"
+	elseif verb == "paste" then
+		if yanked then
+			if is_cut then
+				command = "mv"
+				local force = ""
+				if job.args.force then
+					force = "-f"
+				end
+				args = { force, "-t", cwd, "--", table.unpack(yanked) }
+			else
+				command = "cp"
+				local force = ""
+				if job.args.force then
+					force = "-f"
+				end
+				args = { "-r", force, "-t", cwd, "--", table.unpack(yanked) }
+			end
+		end
+	elseif verb == "link" then
+		if yanked then
+			command = "ln"
+			local force = ""
 			if job.args.force then
 				force = "-f"
 			end
-			args = { "-t", force, cwd, table.unpack(yanked) }
-		else
-			command = "cp"
-			local force = "-i"
-			if job.args.force then
-				force = "-f"
+			local relative = ""
+			if job.args.relative then
+				relative = "-r"
 			end
-			args = { "-r", force, "-t", cwd, table.unpack(yanked) }
+			args =
+				{ "-s", force, relative, "-t", cwd, "--", table.unpack(yanked) }
 		end
-	elseif yanked and verb == "link" then
-		command = "ln"
-		local force = "-i"
-		if job.args.force then
-			force = "-f"
-		end
-		local relative = ""
-		if job.args.relative then
-			relative = "-r"
-		end
-		args = { "-s", force, relative, "-t", cwd, table.unpack(yanked) }
 	elseif verb == "create" then
 		local value, event = ya.input({
 			title = "sudo create:",
@@ -136,25 +140,67 @@ local entry = function(state, job)
 		if not value or event ~= 1 then
 			return
 		end
-		if string.sub(value, -2, -1) == "/" then
-      -- TODO:
+		if string.sub(value, -1, -1) == "/" then
 			command = "mkdir"
-			args = { "-p", value }
+			args = { "-p", "--", value }
 		else
 			command = "touch"
-			args = { value }
+			args = { "--", value }
 		end
+	elseif verb == "rename" then
+		if #files ~= 1 then
+			notify("error", "can only sudo rename one file")
+			return
+		end
+		local value, event = ya.input({
+			title = "sudo rename:",
+			value = Url(files[1]):name(),
+			position = { "hovered", y = 1, w = 40 },
+		})
+		if not value or event ~= 1 then
+			return
+		end
+		command = "mv"
+		args = { "--", table.unpack(files), cwd .. "/" .. value }
+	elseif verb == "chmod" then
+		local value, event = ya.input({
+			title = "sudo chmod (recursive):",
+			position = { "top-center", w = 40 },
+		})
+		if not value or event ~= 1 then
+			return
+		end
+		command = "chmod"
+		args = { "-R", "--", value, table.unpack(files) }
+	-- elseif verb == "open" then
+	-- 	if files then
+	-- 		command = "vi"
+	-- 		args = {
+	-- 			"-u",
+	-- 			"$HOME/.config/nvim/init.lua",
+	-- 			"--",
+	-- 			table.unpack(files),
+	-- 		}
+	-- 	end
 	else
-    ya.err("fail", "files", files, "yanked", yanked, "is_cut", is_cut, "cwd", cwd)
+		notify("error", "plugin needs arguments")
+		return
 	end
 
+	ya.err("HERE", command, args, cwd)
 	local output = run_with_sudo(command, args, cwd)
-	if not output then
-		return ya.err("Failed to run `sudo ls -l`")
-	end
 
 	ya.err("stdout", output.stdout)
+	ya.err("stderr", output.stderr)
 	ya.err("status.code", output.status.code)
+
+	if output.stdout ~= "" then
+		notify("info", {"Ran command:", output.stdout})
+	end
+	if not output.status.success then
+		notify("error", {"Command failed:", output.stderr})
+		return
+	end
 end
 
 return {
