561167288f2f284edfb981ec82eff4b6c6a183c0
salomo <salomo.pflugradt@secunet.com>
2025-02-10 17:46:01 +0100

    feat(keyboard-layout): added instructions in readme and udev rules for nixos

---
+++ keyboard-layout/udev.nix
@@ -0,0 +1,21 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  udev-rule = pkgs.stdenv.mkDerivation (finalAttrs: {
+    pname = "udev-rule-for-via";
+    version = "2025-10-02";
+    src = ./udev-rule;
+    installPhase = ''
+      cp -r . $out
+    '';
+  });
+in
+{
+  services.udev.packages = [
+    udev-rule
+  ];
+}
