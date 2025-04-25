a9d45f7e25d5347dc69d78e2a1a0e78b8bd4e5fb
aMOPel <momoPFL@gmail.com>
2025-04-25 16:19:51 +0200

    fix: reduce workload lsp workload

--- home-manager/modules/nvim/filetypePackages/lua/ft/ftconfigs/css.lua
+++ home-manager/modules/nvim/filetypePackages/lua/ft/ftconfigs/css.lua
@@ -6,19 +6,19 @@ utils.addTable(g.lsp.fts, {
 })
 
 utils.addTable(g.lsp.servers.lsp_installer, {
-	tailwindcss = function(on_attach, capabilities)
-		local add_on_attach = function(client, bufnr)
-			client.server_capabilities.document_formatting = false
-			client.server_capabilities.document_range_formatting = false
-			on_attach(client, bufnr)
-		end
-
-		return {
-			filetypes = { "javascript", "typescript", "jsx", "tsx", "html" },
-			capabilities = capabilities,
-			on_attach = add_on_attach,
-		}
-	end,
+	-- tailwindcss = function(on_attach, capabilities)
+	-- 	local add_on_attach = function(client, bufnr)
+	-- 		client.server_capabilities.document_formatting = false
+	-- 		client.server_capabilities.document_range_formatting = false
+	-- 		on_attach(client, bufnr)
+	-- 	end
+	--
+	-- 	return {
+	-- 		filetypes = { "javascript", "typescript", "jsx", "tsx", "html" },
+	-- 		capabilities = capabilities,
+	-- 		on_attach = add_on_attach,
+	-- 	}
+	-- end,
 	cssls = function(on_attach, capabilities)
 		local add_on_attach = function(client, bufnr)
 			client.server_capabilities.document_formatting = false
