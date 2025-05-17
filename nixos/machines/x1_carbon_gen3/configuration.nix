7d7ddc52bf53df97e4f5c1f0c751a7dafd01ac49
aMOPel <momoPFL@gmail.com>
2025-05-17 22:34:07 +0200

    added config for x1_carbon_gen3

---
+++ nixos/machines/x1_carbon_gen3/configuration.nix
@@ -0,0 +1,49 @@
+let
+  device = "/dev/disk/by-uuid/b7406ebd-2255-4d70-acf2-5a537ea7e82a";
+  personal-info = import ./personal_info.nix;
+  sources = import ../../../nix/sources.nix;
+  pkgs = import sources."nixpkgs_nixos" { };
+  hm = import sources.home-manager { inherit pkgs; };
+  yubikey-disc-encryption = import ../../yubikey-disc-encryption.nix { inherit device; };
+  yubikey-support = import ../../yubikey-support.nix { inherit pkgs; };
+in
+# udev-rule = import ../../../keyboard-layout/udev.nix { inherit pkgs; };
+{
+  imports = [
+    ./hardware-configuration.nix
+    ../../common.nix
+    hm.nixos
+    # udev-rule
+    yubikey-support
+    yubikey-disc-encryption
+  ];
+
+  home-manager = {
+    useGlobalPkgs = false;
+    useUserPackages = false;
+    backupFileExtension = "backup";
+    users."${personal-info.username}" = import ../../../home-manager/home.nix {
+      personal-info-path = ./personal_info.nix;
+    };
+  };
+
+  services.gnome.gnome-keyring.enable = pkgs.lib.mkForce false;
+  networking.hostName = personal-info.nixos.hostname;
+
+  programs.ssh.knownHosts = personal-info.nixos.knownHosts;
+
+  users.users."${personal-info.username}" = {
+    isNormalUser = true;
+    extraGroups = [
+      # "audio"
+      # "cups"
+      "docker"
+      # "input"
+      "libvirtd"
+      # "lp"
+      # "video"
+      "wheel"
+      # "wireshark"
+    ];
+  };
+}
