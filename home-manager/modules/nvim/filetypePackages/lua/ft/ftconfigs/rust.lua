0c2b46350be315c9a278315b56a82a99a1548fff
aMOPel <momoPFL@gmail.com>
2025-08-21 15:33:28 +0200

    add clippy linter to nvim

--- home-manager/modules/nvim/filetypePackages/lua/ft/ftconfigs/rust.lua
+++ home-manager/modules/nvim/filetypePackages/lua/ft/ftconfigs/rust.lua
@@ -8,6 +8,18 @@ utils.addTable(g.lsp.servers.lsp_installer, {
 	rust_analyzer = "default",
 })
 
+utils.addTable(g.linter.filetype, {
+	[ft] = { "clippy_custom" },
+})
+
+utils.addTable(g.linter.custom_linter, {
+	clippy_custom = (function()
+		local clippy_custom = require("lint").linters.clippy
+		clippy_custom.ignore_exitcode = true
+		return clippy_custom
+	end)(),
+})
+
 -- utils.addTable(g.dap.filetype, {
 -- 	"",
 -- })
