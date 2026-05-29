01cf9eb608223d378a4afa67e24d83072c263d8d
aMOPel <momoPFL@gmail.com>
2026-05-29 17:44:54 +0200

    improve yazi config

--- home-manager/modules/yazi/default.nix
+++ home-manager/modules/yazi/default.nix
@@ -6,48 +6,28 @@
 # need to add sudo mappings
 # sudo plugin rename broken
 let
-  # official-plugins = pkgs.fetchFromGitHub {
-  #   owner = "yazi-rs";
-  #   repo = "plugins";
-  #   rev = "8f1d9711bcd0e48af1fcb4153c16d24da76e732d";
-  #   sha256 = "";
-  # };
-
-  # patched-official-plugins = pkgs.stdenv.mkDerivation (finalAttrs: {
-  #   pname = "yazi-rs-plugins-patched";
-  #   version = "2025-01-26";
-  #
-  #   src = official-plugins;
+  # my-sudo-demo = pkgs.stdenv.mkDerivation (finalAttrs: {
+  #   pname = "my-sudo-demo";
+  #   version = "2025-03-02";
+  #   src = ./sudo-demo.yazi;
   #
   #   installPhase = ''
-  #     rm ./sudo-demo.yazi/init.lua
-  #     cp ${./sudo.lua} ./sudo-demo.yazi/init.lua
   #     cp -r . $out
   #   '';
   # });
 
-  my-sudo-demo = pkgs.stdenv.mkDerivation (finalAttrs: {
-    pname = "my-sudo-demo";
-    version = "2025-03-02";
-    src = ./sudo-demo.yazi;
-
-    installPhase = ''
-      cp -r . $out
-    '';
-  });
-
-  # relative-motions = pkgs.fetchFromGitHub {
-  #   owner = "dedukun";
-  #   repo = "relative-motions.yazi";
-  #   rev = "df97039a04595a40a11024f321a865b3e9af5092";
-  #   sha256 = "sha256-csX8T2a5f7k6g2mlR+08rm0qBeWdI4ABuja+klIvwqw=";
+  # sudo = pkgs.fetchFromGitHub {
+  #   owner = "TD-sky";
+  #   repo = "sudo.yazi";
+  #   rev = "f4030083a8a4d1de66f88a8add27ec47b43b01c6";
+  #   sha256 = "sha256-IKdDhLzQCWT8mGNnAbjguoIqxQKUO7N5NsHx51erjLk=";
   # };
-  # compress = pkgs.fetchFromGitHub {
-  #   owner = "KKV9";
-  #   repo = "compress.yazi";
-  #   rev = "60b24af23d1050f1700953a367dd4a2990ee51aa";
-  #   sha256 = "sha256-Yf5R3H8t6cJBMan8FSpK3BDSG5UnGlypKSMOi0ZFqzE=";
-  # };
+  cd-git-root = pkgs.fetchFromGitHub {
+    owner = "ciarandg";
+    repo = "cd-git-root.yazi";
+    rev = "a0e0f4bef420c392f599569dda770f1827569855";
+    sha256 = "sha256-W2ByLHTFN8aMv1d3UfS7FfU7hkKU97rIKWJAHPyKLy0=";
+  };
   copy-file-contents = pkgs.fetchFromGitHub {
     owner = "AnirudhG07";
     repo = "plugins-yazi";
@@ -55,17 +35,17 @@ let
     sha256 = "sha256-JsQJg/SfXLQ/JIpl2YsfzdGpS1ZeWIACJwWTpHaVH3w=";
   };
 in
-# sudo = pkgs.fetchFromGitHub {
-#   owner = "TD-sky";
-#   repo = "sudo.yazi";
-#   rev = "f4030083a8a4d1de66f88a8add27ec47b43b01c6";
-#   sha256 = "sha256-IKdDhLzQCWT8mGNnAbjguoIqxQKUO7N5NsHx51erjLk=";
-# };
 {
   home.packages = with pkgs; [
     ffmpegthumbnailer
     p7zip
+    jq
+    fd
+    ripgrep
+    fzf
+    zoxide
     poppler
+    resvg
     imagemagick
     mediainfo
   ];
@@ -73,21 +53,6 @@ in
   home.sessionVariables = {
     YAZI_LOG = "debug";
   };
-
-  # programs.bash = {
-  #   bashrcExtra = ''
-  #     function y() {
-  #       local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
-  #       yazi "$@" --cwd-file="$tmp"
-  #       if cwd="$(command cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
-  #         builtin cd -- "$cwd"
-  #       fi
-  #       rm -f -- "$tmp"
-  #     }
-  #
-  #     alias ya=yazi
-  #   '';
-  # };
 
   programs.yazi = {
     enable = true;
@@ -98,11 +63,12 @@ in
       "mount" = mount;
       "git" = git;
       "chmod" = chmod;
-      "sudo-demo" = my-sudo-demo;
+      # "sudo-demo" = my-sudo-demo;
       "relative-motions" = relative-motions;
-      "sudo" = sudo;
       "compress" = compress;
       "copy-file-contents" = "${copy-file-contents}/copy-file-contents.yazi";
+      "mediainfo" = mediainfo;
+      "cd-git-root" = cd-git-root;
     };
     initLua = ./init.lua;
 
--- home-manager/modules/yazi/init.lua
+++ home-manager/modules/yazi/init.lua
@@ -15,6 +15,10 @@ th.git.updated_sign = "U"
 
 require("git"):setup()
 
+require("zoxide"):setup({
+  update_db = true,
+})
+
 require("relative-motions"):setup({ show_numbers = "relative", show_motion = true })
 
 require("session"):setup({
