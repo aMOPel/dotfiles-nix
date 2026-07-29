8de98d08e5c1f707260cd0316330fedb72d419c5
aMOPel <momoPFL@gmail.com>
2026-07-29 16:43:48 +0200

    Revert "add vpn and secrets via nix"
    This reverts commit c2b003d0.

--- nixos/configuration/machines/t460s/configuration.nix
+++ nixos/configuration/machines/t460s/configuration.nix
@@ -3,7 +3,6 @@
   pkgs_for_nvim,
   pkgs,
   lib,
-  sops-nix,
   home-manager,
   hmlib,
   ...
@@ -22,7 +21,6 @@ in
     ../../common/yubikey-support.nix
     ./remote-builders.nix
     home-manager.nixosModules.home-manager
-    sops-nix.nixosModules.sops
     # udev-rule
     yubikey-disc-encryption
   ];
@@ -37,11 +35,6 @@ in
       config-values-path = ./config_values.nix;
     };
   };
-
-  sops.age.sshKeyPaths = [ ];
-  sops.gnupg.sshKeyPaths = [ ];
-  sops.age.generateKey = false;
-  sops.gnupg.home = "/home/${config-values.username}/.gnupg";
 
   programs.nix-ld.enable = true;
   programs.nix-ld.libraries = with pkgs; [
