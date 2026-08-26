1f326a459dcfa549742e6d93ba078c42e3a323a4
aMOPel <momoPFL@gmail.com>
2026-08-26 17:02:11 +0200

    vm setup

--- nixos/configuration/machines/t460s/configuration.nix
+++ nixos/configuration/machines/t460s/configuration.nix
@@ -46,6 +46,14 @@ in
     gcc-unwrapped
   ];
 
+  virtualisation.libvirtd = {
+    enable = true;
+    qemu = {
+      swtpm.enable = true;
+    };
+  };
+  programs.virt-manager.enable = true;
+
   services.gnome.gnome-keyring.enable = lib.mkForce false;
   networking.hostName = config-values.nixos.hostname;
 
